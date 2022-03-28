816b99375fad8d7f95e965fea851e0198da02289
GeorgyDev11111 <12nasta60@gmail.com>
2022-03-28 17:50:53 +0500

    add validation

--- src/script/my.js
+++ src/script/my.js
@@ -21,3 +21,33 @@ function removeChild () {
   event.preventDefault();
   document.querySelector('.content').lastElementChild.remove();
 }
+/* Валидация  */
+ let sub = document.querySelector('.sub');
+
+ const category1 = document.getElementById('category1'),
+     category2 = document.getElementById('category2'),
+
+     bid1 = document.getElementById('bid1'),
+     bid2 = document.getElementById('bid2'),
+     bid3 = document.getElementById('bid3');
+
+
+sub.addEventListener('click' , () => {
+  event.preventDefault();
+  const title = document.querySelector('.title').value;
+
+  if(title == ''){
+    alert('Заполниет полe: Заголовок')
+    
+  }else{
+    if(category1.checked || category2.checked){
+      if(bid1.checked || bid2.checked || bid3.checked){
+        alert('ОК 200');
+      }else{
+        alert('Заполниет поле: Вид заявки ');
+      }
+    }else{
+      alert('Заполниет поле: Категория ');
+    }
+  }
+});
